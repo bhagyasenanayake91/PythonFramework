5d2ffe2a0aabcc6e0be845845efe05b2bc0a6c76
bnsenanayake91 <bnsenanayake04@gmail.com>
2024-11-19 14:08:14 +1000

    github workflow

--- conftest.py
+++ conftest.py
@@ -1,65 +1,36 @@
 # import pytest
-# import base64
-
-# from Pages.LoginPage import LoginPage
-# from Utilities.read_config import AppConfiguration
 # from playwright.sync_api import sync_playwright
-# from Pages.ProductsListPage import ProductsListPage
+# from requests import request
 
-# @pytest.hookimpl(hookwrapper=True)
-# def pytest_runtest_makereport(item):
-#     """
-#     Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-#     :param item:
-#     """
-#     screenshot_bytes = ''
-#     pytest_html = item.config.pluginmanager.getplugin('html')
-#     outcome = yield
-#     report = outcome.get_result()
-#     extra = getattr(report, 'extra', [])
+# def pytest_addoption(parser):
+#     parser.addoption("--headed", action="store_true", default=True, help="Run browser in headed mode")
 
-#     if report.when == 'call' or report.when == "setup":
-#         xfail = hasattr(report, 'wasxfail')
 
-#         if report.failed or xfail and "page" in item.funcargs:
-#             page = item.funcargs["setup"]
-
-#             screenshot_bytes = page.screenshot()
-#             extra.append(pytest_html.extras.image(base64.b64encode(screenshot_bytes).decode(), ''))
-
-#         report.extras = extra
-
-# @pytest.fixture()
-# def setup(request):
-#     configuration = AppConfiguration.get_app_configuration()
-#     common_info = AppConfiguration.get_common_info()
-#     base_url = common_info["Url"]
-
-#     # Browser options
-#     headless = eval(configuration["Headless"])  # convert to bool
-#     slow_mo = float(configuration["SlowMo"])
-#     launch_options = {"headless": headless, "slow_mo": slow_mo}
-
-#     # Start Playwright
-#     playwright = sync_playwright().start()
-#     browser = playwright.chromium.launch(**launch_options, args=['--start-maximized'])
-
-#     context_options = {'base_url': base_url}
-
-#     # Browser context settings
-#     browser_context = browser.new_context(**context_options, no_viewport=True)
-#     browser_context.set_default_navigation_timeout(float(configuration["DefaultNavigationTimeout"]))
-#     browser_context.set_default_timeout(float(configuration["DefaultTimeout"]))
-
-#     # Create Page
-#     page = browser_context.new_page()
-
-#     request.cls.page = page
-#     page.goto(base_url)
-
-#     yield page
-#     page.close()
-#     browser.close()
-#     playwright.stop()
-
+#     @pytest.fixture(scope="session")
+#     def playwright():
+#         with sync_playwright() as playwright_instance:
+#             yield playwright_instance
+    
+    
+#     @pytest.fixture(scope="session")
+#     def browser(playwright):
+#         headless_mode = request.config.getoption("--headed")
+#         browser = playwright.firefox.launch(headless=headless_mode)  # Set headless=True for headless mode
+#         yield browser
+#         browser.close()
+    
+#     @pytest.fixture(scope="function")
+#     def browser_context(browser):
+#         context = browser.new_context()
+#         yield context
+#         context.close()
+    
+    
+#     @pytest.fixture(scope="function")
+#     def set_up_tear_down(browser_context):
+#         page = browser_context.new_page()
+#         page.set_viewport_size({"width": 1536, "height": 800})
+#         page.goto("https://www.saucedemo.com/")
+#         yield page
+#         page.close()
     
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,32 +1,16 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect
 
 from pages.HomePage import Login
-
 
 user = {
     "username": "btest1",
     "password": "Test123"
 }
 
-# with sync_playwright() as p:
-#     browser = p.chromium.launch(headless=False)
-#     page = browser.new_page()
-#     login_page = Login(page)
-#     login_page.navigate()
-#     login_page.submit_login(user)
-#     expect(page.locator('//*[@id="showOverview"]/h1')).to_contain_text('Accounts Overview')
-
-#     browser.close()
 class TestLogin:
     def test_login(self, page):
-        
-        # browser = pagechromium.launch(headless=False)
-        # page = browser.new_page()
         login_page = Login(page)
         login_page.navigate()
         login_page.submit_login(user)
         expect(page.locator('//*[@id="showOverview"]/h1')).to_contain_text('Accounts Overview')
-
-        # browser.close()
-
 
